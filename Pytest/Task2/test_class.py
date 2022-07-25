2f52c43431913444636e26d69642e2ed99137398
Maciej <siendobry@gmail.com>
2022-07-25 18:54:48 +0200

    Added tests for TestMaxi class

--- Pytest/Task2/test_class.py
+++ Pytest/Task2/test_class.py
@@ -26,7 +26,6 @@ class TestMini:
     def test_8(self):
         assert is_prime(6) == False
 
-
     # Napisać testy sprawdzające funkcję is_prime dla liczb
     # z przedziału <-1, 6>
 
@@ -47,11 +46,26 @@ class TestMid:
     def test_5(self):
         assert is_prime(65717) == True
 
-
     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
     # liczb z przedziału <10 ** 4, 10 ** 5>
 
-# class TestMaxi:
+class TestMaxi:
+
+    def test_1(self):
+        assert is_prime(1109115290963) == False
+
+    def test_2(self):
+        assert is_prime(1650490569913) == True
+
+    def test_3(self):
+        assert is_prime(4246034826881) == False
+
+    def test_4(self):
+        assert is_prime(7345159501721) == True
+
+    def test_5(self):
+        assert is_prime(9366188049988) == False
+
     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
     # z przedziału <10 ** 12, 10 ** 13>, a następnie
     # naprawić funkcję is_prime aby testy wykonywały się szybciej
