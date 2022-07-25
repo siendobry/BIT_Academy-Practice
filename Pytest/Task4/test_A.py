7176c14b71f886e832bbc2abcc86309ae8942fde
Maciej <siendobry@gmail.com>
2022-07-25 21:36:23 +0200

    Added tests for square function, no skipping yet

--- Pytest/Task4/test_A.py
+++ Pytest/Task4/test_A.py
@@ -1,4 +1,54 @@
+import pytest
 from function import square
+
+
+class TestMini:
+
+    def test_1(self):
+        assert square(-6) == 36
+
+    def test_2(self):
+        assert square(-1) == 1
+
+    def test_3(self):
+        assert square(9) == 81
+
+
+class TestSmall:
+
+    def test_1(self):
+        assert square(-961) == 961**2
+
+    def test_2(self):
+        assert square(-56) == 56**2
+
+    def test_3(self):
+        assert square(225) == 225**2
+
+
+class TestMed:
+
+    def test_1(self):
+        assert square(23454) == 23454**2
+
+    def test_2(self):
+        assert square(654333) == 654333**2
+
+    def test_3(self):
+        assert square(3456456) == 3456456**2
+
+
+@pytest.mark.skipif()           # nie wiem czy o to chodzilo
+class TestMega:
+
+    def test_1(self):
+        assert square(10**12) == 10**24
+
+    def test_2(self):
+        assert square(10**13) == 10**26
+
+    def test_3(self):
+        assert square(10**14) == 10**28
 
 # Napisać 4 klasy funkcji square
 # Pierwsza zawiera kilka testów dla losowych liczb z przedziału <-10, 10>
