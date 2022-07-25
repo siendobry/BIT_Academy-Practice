175fd605886153b583bd32cec80966db83144bba
Maciej <siendobry@gmail.com>
2022-07-25 18:04:08 +0200

    Added 4 tests for Task1

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,16 @@
 from function import square
+
+def test_1():
+    assert square(0) == 0
+
+def test_2():
+    assert square(1) == 1
+
+def test_3():
+    assert square(-1) == 1
+
+def test_4():
+    assert square(100) == 10 ** 4
+
+
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
